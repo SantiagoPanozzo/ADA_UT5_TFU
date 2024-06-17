866dfeb54aa02f43816802a587aa3b27e1394fbc
Maple <sequra128@gmail.com>
2024-06-17 16:52:59 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TFU.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# ADA_UT5_TFU
