a125aaf8032329330613d5c165de0183228da333
Maple <sequra128@gmail.com>
2024-06-18 01:48:48 -0300

    mvc y web

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TFU.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# ADA_UT5_TFU
